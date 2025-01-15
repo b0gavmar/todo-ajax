9a9291c3c4d6860e25eedd482a888c3048c00d02
2020b Gavlik Martin <b0gavmar@vasvari.org>
2025-01-15 11:27:02 +0100

    kiirja a taskokat és usereket

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,32 @@
 <script setup>
   import axios from "axios";
   import {ref, onMounted} from 'vue';
+  const tasks = ref([]);
+  const users = ref([]);
 
   onMounted(() =>{
     axios.get("http://localhost:3000/todos")
-    .then(response => console.log(response))    
-  })
+    .then(response => {
+      console.log(response.data);
+      tasks.value = response.data;
+    })    
+
+    axios.get("http://localhost:3000/users")
+    .then(response => {
+      console.log(response.data);
+      users.value = response.data;
+    })   
+  });
 </script>
 
 <template>
 <h1 class="text-center">Feladatok</h1>
+<div v-for="task in tasks">
+  {{ task.task }}
+</div>
+<hr>
+<div v-for="user in users">
+  {{ user.name }}
+</div>
 </template>
 
